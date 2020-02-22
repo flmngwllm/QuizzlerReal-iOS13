f57a9ae3f75ce11b2db10e943499345f8ebe59ab
flmngwllm <flmngwllm232@gmail.com>
2020-02-22 12:59:08 -0500

    Finish quiz app

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by William Fleming on 2/15/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text : String
+    let answer : String
+    
+    init(q : String, a : String){
+        text = q
+        answer = a
+    }
+}
+
+
